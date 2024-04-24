0d4716956df0cf396794bd9b8e6b5d5464ac1a5b
sreedhini <sreedhinisree@gmail.com>
2024-04-24 18:22:07 +0530

    commit

--- cart/forms.py
+++ cart/forms.py
@@ -4,5 +4,10 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['rating', 'comment']
+        fields = ['comment', 'rating']
 
+    def clean_rating(self):
+        rating = self.cleaned_data['rating']
+        if rating < 0 or rating > 5:
+            raise forms.ValidationError("Rating must be between 0 and 5.")
+        return rating
